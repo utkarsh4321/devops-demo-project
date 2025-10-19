9f4b8247dbc2d1f03c612fa3b26bfd6687431eee
utkarsh4321 <utkarshsrivastava434@gmail.com>
2025-10-19 16:24:49 +0530

    tes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -30,7 +30,7 @@ function App() {
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde et
             illo quo praesentium modi facilis tenetur ullam eaque. Sapiente
             voluptatibus voluptatum optio id quae corrupti natus eum accusamus
-            ut quo.tuuuss Hello raka
+            ut quo.tuuuss Hello raka ss
           </p>
         </header>
       </div>
