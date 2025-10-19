8ae034f88d3febe828105d7c90a85bb9717a386e
utkarsh4321 <utkarshsrivastava434@gmail.com>
2025-10-19 16:21:06 +0530

    added more changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -30,7 +30,7 @@ function App() {
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde et
             illo quo praesentium modi facilis tenetur ullam eaque. Sapiente
             voluptatibus voluptatum optio id quae corrupti natus eum accusamus
-            ut quo.tuuuss Hello world
+            ut quo.tuuuss Hello raka
           </p>
         </header>
       </div>
